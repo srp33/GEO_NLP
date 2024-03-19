ea69273c1f406b875f77bc6596432f75d0111fc2
Stephen Piccolo <stephen.piccolo.byu@gmail.com>
2024-03-19 11:32:48 -0600

    Refined logic for summarizing manual searches

--- Scripts/summarizeManualSearches.py
+++ Scripts/summarizeManualSearches.py
@@ -13,7 +13,7 @@ with gzip.open(gemma_json_file_path) as gemma_file:
     gemmaSet = set(json.loads(gemma_file.read()).keys())
 
 with gzip.open(out_file_path, "w") as out_file:
-    out_file.write("Query\tSearch_Type\tNum_Top\tRecall\n".encode())
+    out_file.write("Query\tSearch_Type\tNum_Top\tMetric\tValue\n".encode())
 
     for queryDirPath in glob.glob(f"{baseDirPath}/*"):
         query = os.path.basename(queryDirPath)
@@ -21,6 +21,7 @@ with gzip.open(out_file_path, "w") as out_file:
         queryFilePath = f"Queries/{query}"
         with open(queryFilePath) as queryFile:
             queryGeoIDs = queryFile.read().strip().split("\n")
+            numTagged = len(queryGeoIDs)
 
         for searchNumFilePath in glob.glob(f"{queryDirPath}/*"):
             searchNum = os.path.basename(searchNumFilePath)
@@ -39,16 +40,30 @@ with gzip.open(out_file_path, "w") as out_file:
 
                 if len(searchText) > 0:
                     geoIDs = re.findall(r"Accession: (GSE\d+)", searchText)
-                    geoIDsInGemma = [x for x in geoIDs if x in gemmaSet]
+                    #geoIDs = [x for x in geoIDs if x in gemmaSet]
 
-                    for numTop in [20, 50, 200, 500, 1000]:
-                        if len(geoIDsInGemma) < numTop:
-                            continue
+                    if len(geoIDs) > 0:
+                        for numTop in [20, 50, 200, 500, 1000, len(geoIDs)]:
+                            if len(geoIDs) < numTop:
+                                continue
 
-                        matches = [x for x in geoIDsInGemma[:numTop] if x in queryGeoIDs]
-                        numMatches = len(matches)
-                        recall = numMatches / numTop
+                            matches = [x for x in geoIDs[:numTop] if x in queryGeoIDs]
+                            numMatches = len(matches)
 
-                        out_file.write((f"{query}\t{searchType}\t{numTop}\t{recall}\n").encode())
+                            #precision = the fraction of retrieved documents that are relevant
+                            precision = numMatches / numTop
+
+                            #recall = num relevant documents in top n / total relevant documents
+                            #         only calculate if total relevant documents > n
+                            recall = numMatches / numTagged
+
+                            if (precision + recall) == 0:
+                                f1 = "NA"
+                            else:
+                                f1 = 2 * ((precision * recall) / (precision + recall))
+
+                            out_file.write((f"{query}\t{searchType}\t{numTop}\tRecall\t{recall}\n").encode())
+                            out_file.write((f"{query}\t{searchType}\t{numTop}\tPrecision\t{precision}\n").encode())
+                            out_file.write((f"{query}\t{searchType}\t{numTop}\tF1 score\t{f1}\n").encode())
 
 print(f"Saved to {out_file_path}")
