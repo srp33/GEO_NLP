d17e858ae24ded2f07d250cbe827180b137ce7c2
Stephen Piccolo <stephen.piccolo.byu@gmail.com>
2024-05-08 16:29:07 -0600

    Saving manual search items

--- Scripts/summarizeManualSearches.py
+++ Scripts/summarizeManualSearches.py
@@ -7,62 +7,68 @@ import sys
 
 baseDirPath = sys.argv[1]
 gemma_json_file_path = sys.argv[2]
-out_file_path = sys.argv[3]
+out_summary_file_path = sys.argv[3]
+out_items_file_path = sys.argv[4]
 
 with gzip.open(gemma_json_file_path) as gemma_file:
     gemmaSet = set(json.loads(gemma_file.read()).keys())
 
-with gzip.open(out_file_path, "w") as out_file:
-    out_file.write("Query\tSearch_Type\tTop_Num\tMetric\tValue\n".encode())
+with gzip.open(out_summary_file_path, "w") as out_summary_file:
+    with gzip.open(out_items_file_path, "w") as out_items_file:
+        out_summary_file.write("Query\tSearch_Type\tTop_Num\tMetric\tValue\n".encode())
+        out_items_file.write("Query\tSearch_Type\tSeries_ID\n".encode())
 
-    for queryDirPath in glob.glob(f"{baseDirPath}/*"):
-        query = os.path.basename(queryDirPath)
+        for queryDirPath in glob.glob(f"{baseDirPath}/*"):
+            query = os.path.basename(queryDirPath)
 
-        queryFilePath = f"Queries/{query}"
-        with open(queryFilePath) as queryFile:
-            queryGeoIDs = queryFile.read().strip().split("\n")
-            numTagged = len(queryGeoIDs)
+            queryFilePath = f"Queries/{query}"
+            with open(queryFilePath) as queryFile:
+                queryGeoIDs = queryFile.read().strip().split("\n")
+                numTagged = len(queryGeoIDs)
 
-        for searchNumFilePath in glob.glob(f"{queryDirPath}/*"):
-            searchNum = os.path.basename(searchNumFilePath)
+            for searchNumFilePath in glob.glob(f"{queryDirPath}/*"):
+                searchNum = os.path.basename(searchNumFilePath)
 
-            if searchNum == "1":
-                searchType = "Tag ontology term"
-            elif searchNum == "2":
-                searchType = "Tag ontology term plus synonyms"
-            else:
-                searchType = "MeSH term"
+                if searchNum == "1":
+                    searchType = "Tag ontology term"
+                elif searchNum == "2":
+                    searchType = "Tag ontology term plus synonyms"
+                else:
+                    searchType = "MeSH term"
 
-            searchResultsFilePath = f"{searchNumFilePath}/search_results.txt"
+                searchResultsFilePath = f"{searchNumFilePath}/search_results.txt"
 
-            with open(searchResultsFilePath) as searchFile:
-                searchText = searchFile.read().strip()
+                with open(searchResultsFilePath) as searchFile:
+                    searchText = searchFile.read().strip()
 
-                if len(searchText) > 0:
-                    geoIDs = re.findall(r"Accession: (GSE\d+)", searchText)
-                    #geoIDs = [x for x in geoIDs if x in gemmaSet]
+                    if len(searchText) > 0:
+                        geoIDs = re.findall(r"Accession: (GSE\d+)", searchText)
+                        #geoIDs = [x for x in geoIDs if x in gemmaSet]
 
-                    if len(geoIDs) > 0:
-                        for numTop in [20, 50, 200, 500, 1000, len(geoIDs)]:
-                            if len(geoIDs) < numTop:
-                                continue
+                        if len(geoIDs) > 0:
+                            for numTop in [20, 50, 200, 500, 1000, len(geoIDs)]:
+                                if len(geoIDs) < numTop:
+                                    continue
 
-                            matches = [x for x in geoIDs[:numTop] if x in queryGeoIDs]
-                            numMatches = len(matches)
+                                matches = [x for x in geoIDs[:numTop] if x in queryGeoIDs]
+                                numMatches = len(matches)
 
-                            #precision = the fraction of retrieved documents that are relevant
-                            precision = numMatches / numTop
+                                #precision = the fraction of retrieved documents that are relevant
+                                precision = numMatches / numTop
 
-                            #recall = num relevant documents in top n / total relevant documents
-                            recall = numMatches / numTagged
+                                #recall = num relevant documents in top n / total relevant documents
+                                recall = numMatches / numTagged
 
-                            if (precision + recall) == 0:
-                                f1 = 0.0
-                            else:
-                                f1 = 2 * ((precision * recall) / (precision + recall))
+                                if (precision + recall) == 0:
+                                    f1 = 0.0
+                                else:
+                                    f1 = 2 * ((precision * recall) / (precision + recall))
 
-                            out_file.write((f"{query}\t{searchType}\t{numTop}\tRecall\t{recall:.2f}\n").encode())
-                            out_file.write((f"{query}\t{searchType}\t{numTop}\tPrecision\t{precision:.2f}\n").encode())
-                            out_file.write((f"{query}\t{searchType}\t{numTop}\tF1 score\t{f1:.2f}\n").encode())
+                                out_summary_file.write((f"{query}\t{searchType}\t{numTop}\tRecall\t{recall:.2f}\n").encode())
+                                out_summary_file.write((f"{query}\t{searchType}\t{numTop}\tPrecision\t{precision:.2f}\n").encode())
+                                out_summary_file.write((f"{query}\t{searchType}\t{numTop}\tF1 score\t{f1:.2f}\n").encode())
 
-print(f"Saved to {out_file_path}")
+                        for geoID in geoIDs:
+                            out_items_file.write((f"{query}\t{searchType}\t{geoID}\n").encode())
+
+print(f"Saved to {out_summary_file_path} and {out_items_file_path}")
